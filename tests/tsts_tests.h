c48fa82550b2f4afcdd64f37644097ccbc0fcbf2
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-05-17 14:33:20 +0300

    Update tsts_tests.h

--- tests/tsts_tests.h
+++ tests/tsts_tests.h
@@ -45,6 +45,42 @@ TEST(abc, test6)
 {
     solution help = abc(-1,-2,15);
     EXPECT_EQ(64,help.d);
-    EXPECT_TRUE((help.x >= -5)&&(help.x <=-4));
-    EXPECT_TRUE((help.x1 >= 3)&&(help.x1 <=4));
+    EXPECT_EQ(-5,help.x);
+    EXPECT_EQ(3,help.x1);
+}
+
+
+TEST(abc, test7)
+{
+    solution help = abc(4,0,0);
+    EXPECT_EQ(0,help.d);
+    EXPECT_EQ(0,help.x);
+}
+
+
+TEST(abc, test8)
+{
+    solution help = abc(0,3,0);
+    EXPECT_EQ(-1,help.d);
+}
+
+
+TEST(abc, test9)
+{
+    solution help = abc(0,0,2);
+    EXPECT_EQ(-1,help.d);
+}
+
+TEST(abc, test10)
+{
+    solution help = abc(0,5,2);
+    EXPECT_EQ(-1,help.d);
+}
+
+TEST(abc, test11)
+{
+    solution help = abc(5,0,-20);
+    EXPECT_EQ(400,help.d);
+    EXPECT_EQ(2,help.x);
+    EXPECT_EQ(-2,help.x1);
 }
