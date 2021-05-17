05642e060dc9ca63dbf9d56887a7ab3547472a6b
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-05-17 12:42:20 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -11,7 +11,7 @@ int main()
     printf("Введите a,b,c: \n");
     if((scanf("%lf",&a)==1)&&
     (scanf("%lf",&b)==1)&&
-    (scanf("%lf",&c)==1){
+    (scanf("%lf",&c)==1)){
     printf("(%lf)*x*x+(%lf)*x+(%lf)=0\n",a,b,c);
     }else{
         printf("Failed to read integer.\n");
